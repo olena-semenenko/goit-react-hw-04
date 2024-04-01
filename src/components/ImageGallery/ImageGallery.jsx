a85481ac925a0104eb4b2ab149d07ffb1cf7b245
olena-semenenko <elen.kinakh@gmail.com>
2024-04-01 21:31:18 +0200

    fix 5.0

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, forwardRef, useEffect } from 'react';
+
 import ImageCard from '../ImageCard/ImageCard';
 import css from './ImageGallery.module.css';
 
@@ -8,14 +9,29 @@ const ImageGallery = ({ collection, onPhotoClick, openModal }) => {
     onPhotoClick(url);
   };
 
+  const lastImageRef = useRef(null);
+  useEffect(() => {
+    if (lastImageRef.current) {
+      lastImageRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }, [collection]);
+
   return (
     <ul className={css.list}>
-      {collection.map(item => (
+      {collection.map((item, index) => (
         <li key={item.id} onClick={() => openModalByClick(item.urls.regular)}>
           <ImageCard image={item} />
+          {index === collection.length - 1 && <div ref={lastImageRef} />}
         </li>
       ))}
     </ul>
+    //   <ul className={css.list}>
+    //     {collection.map(item => (
+    //       <li key={item.id} onClick={() => openModalByClick(item.urls.regular)}>
+    //         <ImageCard image={item} />
+    //       </li>
+    //     ))}
+    //   </ul>
   );
 };
 
